4cdbef622c9815f1a27634b7e2b3131fb2249967
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-10-02 00:38:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002360182294"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-100206632809"))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "7078122796"))
